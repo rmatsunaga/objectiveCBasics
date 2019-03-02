e3c1bed046d97d4cb620a87318ea6abc6b21cec6
Rey Matsunaga <matsunagar97@gmail.com>
2019-03-01 16:28:49 -0800

    Basic operations for collections objects

--- ObjectiveCBasics/ViewController.m
+++ ObjectiveCBasics/ViewController.m
@@ -18,25 +18,66 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    NSString *productTitle = @"Bouncey Balls";
+    /*
+     UNDERSTANDING BASIC TYPES AND OBJECTS
+     */
     
-    NSString *manufacturer = @"Acme Novelty";
     
-    int quantity = 144;
+//    NSString *productTitle = @"Bouncey Balls";
+//
+//    NSString *manufacturer = @"Acme Novelty";
+//
+//    int quantity = 144;
+//
+//    float price = 19.99;
+//
+//    double unitPrice = price/quantity;
+//
+//    NSNumber *unitPriceObject = [NSNumber numberWithDouble:unitPrice];
+//
+//    NSArray *priceArray = @[unitPriceObject];
+//
+//    double test = [unitPriceObject doubleValue] + 5;
+//
+//    NSString *blurb = [NSString stringWithFormat:@"You have selected %@, made by %@.\r\n %@ come in quantity of %i, for a price of $%.2f and a unit price of $%.2f.", productTitle, manufacturer, productTitle, quantity, price, unitPrice];
+//
+//    NSLog(blurb);
     
-    float price = 19.99;
     
-    double unitPrice = price/quantity;
+    NSDictionary *bostonDict = @{@"City": @"Boston", @"Country": @"USA"};
     
-    NSNumber *unitPriceObject = [NSNumber numberWithDouble:unitPrice];
+    NSMutableDictionary *mutBostonDict = [[NSMutableDictionary alloc] initWithDictionary:bostonDict];
+    // OR you can simply initialize a mutable dictionary like so...
+    NSMutableDictionary *bostonAlternativeDict = [[NSMutableDictionary alloc] initWithObjectsAndKeys:@"Boston", @"City", @"USA", @"Country", nil];
     
-    NSArray *priceArray = @[unitPriceObject];
+    NSString *beanTown = [bostonDict valueForKey:@"City"];
     
-    double test = [unitPriceObject doubleValue] + 5;
+    [mutBostonDict setValue: @"Brazil" forKey: @"Country"];
+    [mutBostonDict addEntriesFromDictionary:@{@"Population": @(656000)}];
     
-    NSString *blurb = [NSString stringWithFormat:@"You have selected %@, made by %@.\r\n %@ come in quantity of %i, for a price of $%.2f and a unit price of $%.2f.", productTitle, manufacturer, productTitle, quantity, price, unitPrice];
+    NSMutableDictionary *atlDict = [[NSMutableDictionary alloc] initWithObjectsAndKeys:@"Atlantis", @"City", @"Spain", @"Country", @(150000), @"Population", nil];
+    NSArray *cities = @[bostonDict, atlDict];
     
-    NSLog(blurb);
+    int i = [cities count];
+    int j = cities.count;
+    
+    NSMutableDictionary *sunkenCity = [cities objectAtIndex:1];
+    
+    NSString *sunkenCityName = [[cities objectAtIndex:1] valueForKey:@"City"];
+    int pop = [[[cities objectAtIndex: 1] valueForKey:@"Population"] intValue];
+    
+    NSMutableArray *nameArray = [[NSMutableArray alloc] initWithObjects:@"Rey", @"Deepak", @"Michael", @"Siva", @"Ravi", @"Ajai", @"Anand", nil];
+    
+    [nameArray addObject: @"GANG"];
+    
+    [nameArray removeObjectAtIndex:[nameArray count] - 1];
+    
+    [nameArray replaceObjectAtIndex:0 withObject:"Best Intern Ever, Deathray aka Chad Michaels aka Rey Matsunaga"];
+    
+    NSArray <NSString *> * stringArray;
+    
+    
+    
     
     
 }
