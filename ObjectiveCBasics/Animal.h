6f1671bbba114a070ac9f1155b50563ebf5ec413
Rey Matsunaga <matsunagar97@gmail.com>
2019-03-05 13:25:25 -0800

    interface for class

---
+++ ObjectiveCBasics/Animal.h
@@ -0,0 +1,33 @@
+//
+//  Animal.h
+//  ObjectiveCBasics
+//
+//  Created by Rey Matsunaga on 3/5/19.
+//  Copyright © 2019 Rey Matsunaga. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+enum Group {
+    Mammal = 0,
+    Bird,
+    Reptile,
+    Amphibian,
+    BonyFish,
+    CartFish
+};
+
+@interface Animal : NSObject
+    
+    @property (nonatomic, strong) NSString *name;
+    @property (nonatomic,) enum Group group;
+    @property (nonatomic) BOOL isExtinct;
+    @property (nonatomic) IBOutlet UIImageView *thumbnailView;
+    
+    
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ObjectiveCBasics/Animal.m
@@ -0,0 +1,13 @@
+//
+//  Animal.m
+//  ObjectiveCBasics
+//
+//  Created by Rey Matsunaga on 3/5/19.
+//  Copyright © 2019 Rey Matsunaga. All rights reserved.
+//
+
+#import "Animal.h"
+
+@implementation Animal
+
+@end
